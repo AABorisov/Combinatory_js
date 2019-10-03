d6e66c9708a4705e05b41ecf4d2bf24b50edddfc
AABorisov <borisov.andrew.a@gmail.com>
2019-10-03 03:21:39 +0300

    heapPermutate numberByPermutation

--- numberByPermutation.js
+++ numberByPermutation.js
@@ -12,6 +12,8 @@ for (let i = 0; i < n; i++) {
         }
     }
 }
+let dd = permutation.map( ( iPerm, i, arr ) => arr.slice( i + 1 ).reduce( ( acc, jPerm ) => acc + ( jPerm < iPerm ? 1 : 0 ), 0 ) )
 
 const result = d.reduce( ( acc, valD, index ) => acc + valD * f[n - index - 1], 1)
-console.log(permutation, d, result, f)
+const result2 = dd.reduce( ( acc, valD, index ) => acc + valD * f[n - index - 1], 1)
+console.log(permutation, d, result, f, result2)
